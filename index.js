0dddf5bbcb799b924b6dd54930d9a09fce57d2a3
gr0bs <gr0bs@outlook.com>
2021-11-29 21:19:56 +0100

    Meteo

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 import TelegramBot  from 'node-telegram-bot-api'
 import dotenv from 'dotenv'
+import moment from 'moment'
+import fetch from 'node-fetch'
 dotenv.config()
 
 const token = process.env.BOT_TOKEN
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
+setInterval(async () => {
+  let weather = {
+    temp: null,
+    min: null,
+    max: null
+  }
+  const HOUR = moment().local(true).format('HH:mm')
+  // bot.on('message', (msg) => {
+  //   const chatId = msg.chat.id;
+  if(HOUR === '21:18'){
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${process.env.CITY}&appid=${process.env.API}`)
+      const data = await response.json()
+      weather.temp = (parseFloat(data.main.temp) - 273.15).toFixed(1)
+      weather.min = (parseFloat(data.main.temp_min) - 273.15).toFixed(1)
+      weather.max = (parseFloat(data.main.temp_max) - 273.15).toFixed(1)
+      // });
+    }catch(err){
+      console.log(err)
+    }
+    
+    if(weather.temp !== null){
+      bot.sendMessage('509416027', `Il fait actuellement ${weather.temp}°C. Mais attention, il fera minimum ${weather.min}°C et maximum ${weather.max}°C. Bonne journée :) `);
+      console.log(moment().local(true).format("HH:mm"))
+    } else {
+      bot.sendMessage('509416027', `Bravo.. Bravo ! Je n'ai pas regardé la météo..`);
+    }
+  }
+},3000)
+// 3600000
