b7f011244e4ebe2549cc54fdf24c21e6d9f35999
gr0bs <gr0bs@outlook.com>
2022-03-11 22:30:57 +0100

    update

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ dotenv.config()
 const token = process.env.BOT_TOKEN
 const bot = new TelegramBot(token, {polling: true});
 
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
 setInterval(async () => {
   let weather = {
     temp: null,
